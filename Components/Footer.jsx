d5998a14e4123244e64896ceea9fc317ed71c386
Ankesh Yadav <92791586+ankesh1111@users.noreply.github.com>
2022-08-24 20:58:40 +0530

    updated

---
+++ Components/Footer.jsx
@@ -0,0 +1,59 @@
+
+ import footer from "../assets/footer.png"
+
+function Footer(){
+
+    return (
+        <div>
+  <div id="container4">
+          <div class="c1">
+            <h5>Info</h5>
+            <p class="s1">Search</p>
+            <p class="s2">Terms of Service</p>
+            <p class="s3">Refund policy</p>
+            <p class="s4">Wholesale</p>
+            <p class="s5">Wholesale Signup Form</p>
+            <p >Shipping</p>
+            <h5 class="h5">Subscribe to our emails</h5>
+            <input type="text" placeholder="Email"/>
+
+            {/* <div class="i">
+           < FaFacebook/>
+           <FaInstagramSquare/>
+           <FaTiktok/>
+            <FaPinterestP/>
+            </div> */}
+      </div>
+
+        <div class="c2">
+          <h5 class="d1">Contact Us!</h5>
+          <p>Need to talk? Reach us via email, phone or text!</p>
+          <h4>Email: Customerservice@Lovoda.com</h4>
+          <h4 class="d2">Phone: (443) 500-1200</h4>
+          <p class="d3">**Msg &data rates may apply</p>
+        </div>
+      </div>            
+      
+    <br />
+    <br />
+    <hr />
+            
+                {/* FOOTER SECTION SECOND-2  */}
+      
+       <div id="container5">
+        <div>
+          <img class="l1" src={footer}  />
+        </div>
+
+       </div>
+
+       <p class="a1">@ 2022, Lovoda Powered by Shopify</p>
+
+
+
+
+
+        </div>
+    )
+}
+export default Footer;
---
+++ Components/Navbar.jsx
@@ -0,0 +1,42 @@
+import { Link } from "react-router-dom";
+ import "./Navbar.css"
+ import { FaUserAlt,FaSearch,FaCartArrowDown } from "react-icons/fa";
+ function Navbar(){
+
+    return (
+        <div>
+          
+            <p>Free Shipping on Orders Over $75 and Free Returns (US ONLY)</p>
+
+            <hr />
+            <div id="navbar">
+                
+                
+                <div class="l2">
+                <h2>LOVODA</h2>
+        <p class="e1"><Link to="/">Home</Link> </p>
+        <p class="e1"><Link to="/new">New</Link> </p>
+        <p class="e1"><Link to="/shopall">Shop All</Link> </p>
+        <p class="e1"><Link to="/earning">Earning</Link></p>
+        <p class="e1"><Link to="/necklaces">Necklaces</Link> </p>
+        <p class="e1"><Link to="/bracelets"> Bracelets</Link></p>
+        <p class="e1"><Link to="/rings"> Rings</Link> </p>
+        <p class="e1"><Link to="/shopsocial"> Shop Social</Link></p>
+                </div>
+
+                <div class="l3">
+                   
+                      <p class="fa"><Link to="/search"><FaSearch/></Link></p>
+                      <p class="fa"><Link  to="/Login">< FaUserAlt/></Link></p>
+                      <p class="fa"><Link to="/cart"><FaCartArrowDown/></Link></p>
+                     
+                  
+                   
+
+                </div>
+            </div>
+          
+        </div>
+    )
+}
+export default Navbar;
